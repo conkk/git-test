9d83c9549d2ba2ed9e84218c8c04e4d8b19a6974
xige <xige@qq.com>
2022-02-25 16:02:26 +0800

    first commit

---
+++ src/main/java/com/lianbo/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lianbo.git;
+
+/**
+ * @author lianbo
+ * @create 2022-02-25 15:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
