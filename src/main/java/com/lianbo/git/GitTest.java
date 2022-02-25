359fd68ac191f1a9f854777cbc545d35b12e8a78
xige <xige@qq.com>
2022-02-25 16:15:27 +0800

    hot-fix commit2

--- src/main/java/com/lianbo/git/GitTest.java
+++ src/main/java/com/lianbo/git/GitTest.java
@@ -6,10 +6,12 @@ package com.lianbo.git;
  */
 public class GitTest {
     public static void main(String[] args) {
+
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hot-fix test!");
 
     }
 }
